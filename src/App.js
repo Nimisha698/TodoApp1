c954b94f9794898acc6680bd92d2e5a32415af23
Nimisha <nimisha.sanjay@zemosolabs.com>
2019-06-26 15:44:36 +0530

    adding delete option to todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+class App extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state = {list: [], task: ''};
+    this.handleChange=this.handleChange.bind(this);
+    this.handleClick=this.handleClick.bind(this);
+    this.edit=this.edit.bind(this);
+    this.delete=this.delete.bind(this);
+  }
+  handleChange(event)
+  {
+    this.setState({
+    task:event.target.value
+    }
+    );
+  }
+  handleClick(event)
+  {
+    this.setState({
+      list:[...this.state.list,this.state.task]
+      
+    });
+    console.log(this.state.list);
+  }
+  edit(e)
+  {
+      let a=e.currentTarget.id;
+      e.preventDefault(); 
+      
+  }
+  delete(e){ 
+    let a=e.currentTarget.id;
+    e.preventDefault();  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    for(let i=0;i<this.state.list.length;i++)
+    {
+      if(this.state.list[i]===a)
+      {this.state.list.splice(i,1);
+        this.render();
+      }
+    }
+  }
+  render()
+  {
+    
+    console.log("called")
+    return(
+      <form>
+      <TextField
+        id="outlined"
+        style={{ margin: 8 }}
+        placeholder="Enter task name"
+        margin="normal"
+        variant="outlined"
+        onChange={this.handleChange}
+      />
+      <Button variant="contained" color="primary" onClick={this.handleClick} l={10}>Add Task
+      
+      </Button>
+        <ol>
+          {this.state.list.map(item => (
+            <li key={item}>{item}  <Button variant="outlined" color="primary" id={item} onClick={this.delete}>Edit</Button>
+            <Button variant="outlined" color="primary" id={item} onClick={this.delete}>Delete</Button>
+          
+            </li>
+          ))}
+ 
+        </ol>
+      </form>
+    );
+  }
 }
-
 export default App;
